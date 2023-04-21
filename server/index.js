c8521a657d77ee319c581dbcf0a4ff79086384aa
mauricio dall onder <131299118+londer654@users.noreply.github.com>
2023-04-21 11:26:22 -0300

    segundo

--- server/index.js
+++ server/index.js
@@ -1,33 +1,94 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
-
-dotenv.config();
-
+import express from "express";
+import fetch from "node-fetch";
+import jwt from "jsonwebtoken";
+import cors from "cors";
 const app = express();
+
+app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+const API_KEY = "619f741d-eabe-4741-882a-404b0b023de6";
+const SECRET =
+  "573c2eb460d7bbafa9f65c954fc33164c2c127d6eaca26196e00c2835c49ebc9";
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+function generateToken() {
+  const options = {
+    expiresIn: "120m",
+    algorithm: "HS256",
+  };
 
-app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'Hello from DALL.E!',
-  });
+  const payload = {
+    apikey: API_KEY,
+    permissions: ["allow_join"], // `ask_join` || `allow_mod`
+    version: 2,
+    roles: ["CRAWLER"],
+  };
+
+  const token = jwt.sign(payload, SECRET, options);
+  return token;
+}
+
+async function deactivateRoom(roomId) {
+    const YOUR_TOKEN = generateToken();
+  
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: YOUR_TOKEN,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ roomId }),
+    };
+  
+    const url = "https://api.videosdk.live/v2/rooms/deactivate";
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+app.post("/create-room", async (req, res) => {
+  const YOUR_TOKEN = generateToken();
+
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: YOUR_TOKEN,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      region: "sg001",
+    }),
+  };
+
+  const url = "https://api.videosdk.live/v2/rooms";
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-const startServer = async () => {
-  try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-startServer();
+app.post("/deactivate-room", async (req, res) => {
+    const roomId = req.body.roomId;
+    if (!roomId) {
+      return res.status(400).json({ error: "Room ID is required" });
+    }
+  
+    try {
+      const response = await deactivateRoom(roomId);
+      res.json(response);
+    } catch (error) {
+      console.error("Error deactivating room:", error);
+      res.status(500).json({ error: "Error deactivating room" });
+    }
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
